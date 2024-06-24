915b91663bf5c5785424db47eb7586fe1674d100
Shivam <techbulls2021@gmail.com>
2024-06-24 08:02:40 +0530

    Completed Register Controller and also Access and Refresh Tokens

--- backend/src/controllers/auth.controller.js
+++ backend/src/controllers/auth.controller.js
@@ -1,17 +1,49 @@
 import { createUser } from "../services/auth.service.js";
+import { generateToken } from "../services/token.service.js";
 
 export const register = async (req, res, next) => {
   try {
     const { name, email, password, status, picture } = req.body;
 
-    const user = await createUser({
+    const newUser = await createUser({
       name,
       email,
       password,
       status,
       picture,
     });
-    res.send(user);
+    const access_token = await generateToken(
+      { userId: newUser._id },
+      "1d",
+      process.env.ACCESS_TOKEN_SECRET
+    );
+
+    const refresh_token = await generateToken(
+      { userId: newUser._id },
+      "30d",
+      process.env.REFRESH_TOKEN_SECRET
+    );
+
+    res.cookie("refreshToken", refresh_token, {
+      httpOnly: true,
+      path: "/api/v1/auth/refreshtoken",
+      maxAge: 30 * 24 * 60 * 60 * 10000, //30 Days
+    });
+    // console.table({ access_token, refresh_token });
+
+    res.json({
+      message: "register sucessful.",
+
+      user: {
+        _id: newUser._id,
+        name: newUser.name,
+        email: newUser.email,
+        password: newUser.password,
+        picture: newUser.picture,
+        status: newUser.status,
+        access_token: access_token,
+      },
+    });
   } catch (error) {
     next(error);
   }
