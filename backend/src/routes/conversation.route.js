d7c8184fd010c5bca1d730d10d4753f97978106e
Shivam <techbulls2021@gmail.com>
2024-07-07 14:23:45 +0530

    For Pushing

---
+++ backend/src/routes/conversation.route.js
@@ -0,0 +1,10 @@
+import express from "express";
+import trimRequest from "trim-request";
+import authMiddleware from "../middlewares/authMiddleware.js";
+// import create_open_conversation from "../controllers/conversation.controller.js";
+
+const router = express.Router();
+
+router.route("/").post(trimRequest.all, authMiddleware);
+
+export default router;
--- backend/src/routes/index.js
+++ backend/src/routes/index.js
@@ -1,8 +1,10 @@
 import express from "express";
 import authRoutes from "./auth.route.js";
+import conversationRoutes from "./conversation.route.js";
 
 const router = express.Router();
 
 router.use("/auth", authRoutes);
+router.use("/", conversationRoutes);
 
 export default router;
